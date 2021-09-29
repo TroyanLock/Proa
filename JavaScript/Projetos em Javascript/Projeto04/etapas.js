6c19b0a8cc6193edcdf3d87a23fd8a03b5103045
Vinicius Rodrigues <89105371+TroyanLock@users.noreply.github.com>
2021-09-29 14:54:03 -0300

    Add files via upload

---
+++ JavaScript/Projetos em Javascript/Projeto04/etapas.js
@@ -0,0 +1,140 @@
+let etapas = [
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '38',
+                nome: 'Jair Bolsonaro',
+                partido: 'GUN',
+                vice: 'Mourão',
+                fotos:[
+                    {url:'jair-bolsonaro.jpg', legenda: 'Presidente'},
+                    {url:'mourao.jpg', legenda: 'Vice-Presidente',  small: true}
+
+                ]
+            },
+            {
+                numero: '13',
+                nome: 'Luiz Inácio "lula" Da Silva',
+                partido: 'PT',
+                vice: 'Carla',
+                fotos:[
+                    {url:'lula.jpg', legenda: 'Presidente'},
+                    {url:'vice2.jpg', legenda: 'Vice-Presidente',  small: true}
+
+                ]
+            },
+            {
+                numero: '27',
+                nome: 'Ciro Gomes',
+                partido: 'PDT',
+                vice: 'Gabriel',
+                fotos:[
+                    {url:'ciro-gomes.jpg', legenda: 'Presidente'},
+                    {url:'vice3.jpg', legenda: 'Vice-Presidente',  small: true}
+
+                ]
+            },
+            {
+                numero: '30',
+                nome: 'João amoedo',
+                vice: 'Julia',
+                partido: 'NOVO',
+                fotos:[
+                    {url:'joao-amoedo.jpg', legenda: 'Presidente'},
+                    {url:'vice4.jpg', legenda: 'Vice-Presidente',  small: true}
+
+                ]
+            },
+            {
+                numero: '51',
+                nome: 'Cabo Daciolo',
+                partido: 'PATRIOTA',
+                vice: 'Antônio',
+                fotos:[
+                    {url:'cabo-daciolo.jpg', legenda: 'Presidente'},
+                    {url:'vice5.jpg', legenda: 'Vice-Presidente',  small: true}
+                ]
+            },
+            {
+                numero: '01',
+                nome: 'Renovação',
+                partido: 'Fé',
+                vice: 'Esperança',
+                fotos:[
+                    {url:'teste.jpg', legenda: 'Presidente'},
+                    {url:'gi.jpg', legenda: 'Vice-Presidente',  small: true}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'GOVERNADOR',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '15',
+                nome: 'Lucas',
+                partido: 'PSB',
+                vice: 'José',
+                fotos:[
+                    {url:'g1.jpg', legenda: 'Governador'},
+                    {url:'vice6.jpg', legenda: 'Vice-Governador',  small: true}
+                ]
+            },
+            {
+                numero: '47',
+                nome: 'Wellington',
+                partido: 'PTT',
+                vice: 'Elisa',
+                fotos:[
+                    {url:'g2.jpg', legenda: 'Governador'},
+                    {url:'vice7.jpg', legenda: 'Vice-Governador',  small: true}
+                ]
+            },
+            {
+                numero: '99',
+                nome: 'Wilson',
+                partido: 'QTS',
+                vice: 'Adêmar',
+                fotos:[
+                    {url:'g3.jpg', legenda: 'Governador'},
+                    {url:'vice8.jpg', legenda: 'Vice-Governador',  small: true}
+                ]
+            },
+            {
+                numero: '17',
+                nome: 'Felipe',
+                vice: 'Pedro',
+                partido: 'UPC',
+                fotos:[
+                    {url:'g4.jpg', legenda: 'Governador'},
+                    {url:'vice9.jpg', legenda: 'Vice-Governador',  small: true}
+                ]
+            },
+            {
+                numero: '51',
+                nome: 'Marília',
+                partido: 'SRB',
+                fotos:[
+                    {url:'g5.jpg', legenda: 'Governador'},
+                    {url:'vice1.jpg', legenda: 'Vice-Governador', small: true}
+                ]
+            },
+                {
+                    numero: '01',
+                    nome: 'Paz',
+                    partido: 'Fé',
+                    vice: 'Determinação',
+                    fotos:[
+                        {url:'avatar.jpg', legenda: 'Governador'},
+                        {url:'teste.jpg', legenda: 'Vice-Governador',  small: true}
+                    ]
+                }
+    
+            
+        ]
+
+    }
+]
